e1476b4e3c531f60d4d452b7f353ca12bf047eef
Yik Kok Yong <johnyong@live.cn>
2018-12-16 15:02:12 +0800

    Removed get post by id
    Due to server return 403

--- src/app/action/posts.js
+++ src/app/action/posts.js
@@ -4,8 +4,5 @@ import instance from "./axiosInstance";
 export default {
   fetchPosts: (page: number, per_page: number) => {
     return instance.get(`/wp/v2/posts?_embed&page=${page}&per_page=${per_page}`);
-  },
-  fetchPostByID: (postID: number) => {
-    return instance.get(`/wp/v2/posts?${postID}`);
   }
 };
